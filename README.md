7e6aa215a0fafed5bfd287af00b7174be91fadc0
tankh99 <khanghou99@gmail.com>
2021-12-16 14:31:30 +0800

    delete session

---
+++ README.md
@@ -0,0 +1,12 @@
+# Lesson for scripting on telegram.
+Libraries used: Telethon, giphy api, telegram access.
+
+* Current feature includes: 
+- auto replying with a pre-defined message
+- telegram session
+- Replying with random giphy
+- Calling random giphy and downloading it to local.
+
+
+Basic python and references available at:
+https://github.com/dirtyprototyper/pythonbasics
---
+++ src/main.py
@@ -0,0 +1,35 @@
+import os
+from telethon.sync import TelegramClient, events
+from telethon.tl.types import PeerUser # Import events!
+
+api_id = os.environ.get("telegram_api_id")
+api_hash = os.environ.get("telegram_api_hash")
+phone_number = os.environ.get("phone_number") 
+
+# Create the client and connect. Commented out the following 1 line
+# use sequential_updates=True to respond to messages one at a time
+client = TelegramClient(f"anon", api_id, api_hash, sequential_updates=True)
+client.start(phone_number)
+
+
+if __name__ == '__main__':
+    print('Program initiated')
+    client.send_message("me", "Initiating program")
+
+
+    @client.on(events.NewMessage())
+    async def handle_message(event):
+        await client.send_message("me", "Hello myself!") # Make sure to await the send_message function!
+        # await event.respond("Hello myself!!") # Another way to reply. Difference being it will automatically reply to the sender
+        
+        # Exploring Further
+        # await client.send_message("Me", f"You messaged me on {event.message.date}")
+        # user = await event.get_input_sender()
+        user = await client.get_entity(PeerUser(event.message.peer_id.user_id))
+        print(user)
+        await client.send_message("Me", f"You are {user.first_name} {user.last_name} and you messaged me on {event.message.date}")
+        
+    client.run_until_disconnected()
+
+
+
