c00fe7d6475aa46be42e959578234e36106c2fe4
tankh99 <khanghou99@gmail.com>
2021-12-16 14:09:48 +0800

    made some trivial changes

--- src/main.py
+++ src/main.py
@@ -4,15 +4,12 @@ from telethon.sync import TelegramClient
 # You can retrieve the following API information through this website: https://my.telegram.org/apps
 api_id = "<YOUR_API_ID>"
 api_hash = "<YOUR_API_HASH>"
-phone_number = "<YOUR PHONE NUMBER>" # Make sure to includ the country code
+phone_number = "<YOUR PHONE NUMBER>" # Make sure to include the country code
 
 
 # # Create the client and connect. Commented out the following 1 line
-# # use sequential_updates=True to respond to messages one at a time
 client = TelegramClient(f"anon", api_id, api_hash, sequential_updates=True)
 client.start(phone_number)
-
-# client.send_message(me, "Initiating program")
 
 if __name__ == '__main__':
     print('Program initiated')        
