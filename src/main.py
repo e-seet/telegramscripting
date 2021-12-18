7fd6067a031d1cdb582136027877fe04a260a269
tankh99 <khanghou99@gmail.com>
2021-12-18 12:28:46 +0800

    updated master branch

--- src/main.py
+++ src/main.py
@@ -1,18 +1,7 @@
-from telethon.sync import TelegramClient 
-
 # Input the following information
 # You can retrieve the following API information through this website: https://my.telegram.org/apps
 api_id = "<YOUR_API_ID>"
 api_hash = "<YOUR_API_HASH>"
 phone_number = "<YOUR PHONE NUMBER>" # Make sure to include the country code
 
-
-# # Create the client and connect. Commented out the following 1 line
-client = TelegramClient(f"anon", api_id, api_hash, sequential_updates=True)
-client.start(phone_number)
-
-if __name__ == '__main__': # entry point of the application
-    print('Program initiated')
-    client.send_message("me", "Hello world!") # Sends a message to yourself
-    client.run_until_disconnected()
 
