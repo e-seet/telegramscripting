d8458a09e80a798f416c456ebf80b73e21e1533c
dirtyprototyper <dirtyprototyping@gmail.com>
2021-12-18 22:26:06 +0800

    commented out newly added additional features and return it to original status

--- src/main.py
+++ src/main.py
@@ -33,22 +33,22 @@ if __name__ == '__main__':
     print('Program initiated')
     client.send_message("me", "Initiating program")
 
-    # @client.on(events.NewMessage(outgoing=True, pattern=r'.*(hell|heck|frick)')) # Add additional swear words if you want
-    # async def handle_swear(event):
-    #     print(event)
-    #     time.sleep(1)
-    #     await client.edit_message(event.message, "I've been naughty today")
-    #     # await event.delete()
-    #     print("Deleted message!")
+    @client.on(events.NewMessage(outgoing=True, pattern=r'.*(hell|heck|frick)')) # Add additional swear words if you want
+    async def handle_swear(event):
+        print(event)
+        time.sleep(1)
+        await client.edit_message(event.message, "I've been naughty today")
+        # await event.delete()
+        print("Deleted message!")
         
 
-    # @client.on(events.NewMessage(outgoing=True, pattern=r'\.save')) # Reply to someone with .save
-    # async def handle_message(event):
-    #     if event.is_reply: # checks to see if sent message == .save
-    #         replied = await event.get_reply_message()
-    #         sender = replied.sender
-    #         await client.download_profile_photo(sender, f"images/{sender.username}.jpg")
-    #         await event.respond('Saved your photo {}'.format(sender.username))
+    @client.on(events.NewMessage(outgoing=True, pattern=r'\.save')) # Reply to someone with .save
+    async def handle_message(event):
+        if event.is_reply: # checks to see if sent message == .save
+            replied = await event.get_reply_message()
+            sender = replied.sender
+            await client.download_profile_photo(sender, f"images/{sender.username}.jpg")
+            await event.respond('Saved your photo {}'.format(sender.username))
         
     # # Additional features
     # @client.on(events.UserUpdate()) # Occurs whenever a user goes online or starts typing
@@ -63,34 +63,34 @@ if __name__ == '__main__':
     #             await client.send_message(user.username, "I see you online!")
 
 
-    #additonal feature
-    @client.on(events.NewMessage(incoming=True))
-    async def handle_new_message(event):
-        if event.is_private:     
+    #additonal feature. Auto replying to group chat and replying to specifc message in groupchat
+    # @client.on(events.NewMessage(incoming=True))
+    # async def handle_new_message(event):
+    #     if event.is_private:     
             
-            #ignore someone 
-            # if event.message.peer_id.user_id != 41197530:
+    #         #ignore someone 
+    #         # if event.message.peer_id.user_id != 41197530:
 
-                #the reply to private chat
-                if event.message.peer_id.user_id == 876675202: 
-                    await client.send_message(event.message.peer_id,message="Hello")  # this works, send back to private chat
+    #             #the reply to private chat
+    #             if event.message.peer_id.user_id == 876675202: 
+    #                 await client.send_message(event.message.peer_id,message="Hello")  # this works, send back to private chat
 
-        #not privagte
-        else:
-                #check that it is a reply message
-                if event.is_reply == True : 
-                    print(event.original_update.chat_id) #the group id should be t he same as the below 
-                    if event.original_update.chat_id ==  704140264: #the number should be the same as above 
-                        await event.reply("wow")
+    #     #not privagte
+    #     else:
+    #             #check that it is a reply message
+    #             if event.is_reply == True : 
+    #                 print(event.original_update.chat_id) #the group id should be t he same as the below 
+    #                 if event.original_update.chat_id ==  704140264: #the number should be the same as above 
+    #                     await event.reply("wow")
 
-                else:
-                    #both replies back to the group chat
-                    if event.message.peer_id.chat_id == 704140264:
-                        await client.send_message(event.message.peer_id,message="Hi Group")  
+    #             else:
+    #                 #both replies back to the group chat
+    #                 if event.message.peer_id.chat_id == 704140264:
+    #                     await client.send_message(event.message.peer_id,message="Hi Group")  
                         
 
-                    if event.original_update.chat_id == 704140264:
-                        await client.send_message(event.original_update.chat_id,message="Hi Groups")  
+    #                 if event.original_update.chat_id == 704140264:
+    #                     await client.send_message(event.original_update.chat_id,message="Hi Groups")  
                         
 
 
