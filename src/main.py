6813b5cf63c4df3c3a314e6c2df63762ab0dfe6c
tankh99 <khanghou99@gmail.com>
2021-12-18 18:12:35 +0800

    template for step 4. Step 4 is to reply to groups

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 from telethon.sync import TelegramClient, events
 from telethon.tl.types import PeerUser 
+from telethon.tl.functions.messages import ImportChatInviteRequest
 import random # Imported random
 
 api_id = os.environ.get("telegram_api_id")
